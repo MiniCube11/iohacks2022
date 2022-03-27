d34e473480a7605821a326ea75f031e7dba1989f
Ching Lam Lau <contact.minicube11@gmail.com>
2022-03-26 22:55:20 -0400

    Improve home page

--- app/views/main.py
+++ app/views/main.py
@@ -4,17 +4,18 @@ from app.models import Event
 bp = Blueprint('main', __name__)
 
 
+def format_date(date):
+    months = ['', "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return f"{months[date.month]} {date.day}"
+
+
 @bp.route('/')
 def index():
-    return render_template('main/index.html')
+    _events = Event.query.all()
+    return render_template('main/index.html', events=_events, format_date=format_date)
 
 
 @bp.route('/events')
 def events():
     _events = Event.query.all()
-
-    def format_date(date):
-        months = ['', "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        return f"{months[date.month]} {date.day}"
-
     return render_template('main/events.html', events=_events, format_date=format_date)
